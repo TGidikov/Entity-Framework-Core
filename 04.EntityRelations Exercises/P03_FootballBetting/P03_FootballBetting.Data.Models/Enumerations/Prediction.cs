44d4b9a9910c251a0db98a613d317aa7b1a84996
TodorVGidikov <49285731+TGidikov@users.noreply.github.com>
2020-07-10 20:53:53 +0300

    Add files via upload

---
+++ 04.EntityRelations Exercises/P03_FootballBetting/P03_FootballBetting.Data.Models/Enumerations/Prediction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models.Enumerations
+{
+    public enum Prediction
+    {
+        Win=1,
+        Lose=2,
+        Draw=3
+
+    }
+}
---
+++ 04.EntityRelations Exercises/P03_FootballBetting/P03_FootballBetting.Data.Models/User.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Bets = new HashSet<Bet>();
+        }
+        public int UserId { get; set; }
+
+        public string Username { get; set; }
+
+        //HashedPassword
+        public string Password { get; set; }
+
+        public string  Email { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public virtual ICollection<Bet> Bets { get; set; }
+
+
+    }
+}

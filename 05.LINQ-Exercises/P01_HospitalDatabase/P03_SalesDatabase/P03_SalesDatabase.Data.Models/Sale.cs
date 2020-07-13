8718a3e0c23997efbda2893550d5ee911a4c7b63
TodorVGidikov <49285731+TGidikov@users.noreply.github.com>
2020-07-13 12:14:31 +0300

    Add files via upload

---
+++ 05.LINQ-Exercises/P01_HospitalDatabase/P03_SalesDatabase/P03_SalesDatabase.Data.Models/Sale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Sale
+    {
+        [Key]
+        public int SaleId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Required, ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        [Required, ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        [Required,ForeignKey("Store")]
+        public int StoreId { get; set; }
+        public Store Store { get; set; }
+    }
+}
---
+++ 05.LINQ-Exercises/P01_HospitalDatabase/P03_SalesDatabase/P03_SalesDatabase.Data/Migrations/20200713090436_SalesAddDateDefault.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_SalesDatabase.Data.Migrations
+{
+    public partial class SalesAddDateDefault : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Sales",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Sales",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}

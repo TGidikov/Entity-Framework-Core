c08554ea11e93632e0cd9c47c5fa67f4edee07e9
TodorVGidikov <49285731+TGidikov@users.noreply.github.com>
2020-08-20 10:37:03 +0300

    Add files via upload

---
+++ MY EXAM/VaporStore/Data/Models/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using VaporStore.Data.Models.Enums;
+
+namespace VaporStore.Data.Models
+{
+    public class Card
+    {
+        public Card()
+        {
+            this.Purchases = new HashSet<Purchase>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}$")]
+        public string Number { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]{3}$")]
+        public string Cvc { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+        
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        
+        [Required]
+        public User User { get; set; }
+
+        public virtual ICollection<Purchase> Purchases { get; set; }
+    }
+}
